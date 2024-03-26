cc3040911a7dde9ad2363d84e30312b4cabeae56
Alexandr Yarmosh <sasha.yarmosh63@gmail.com>
2024-03-26 11:56:50 +0300

    create project

---
+++ src/main/java/org/example/DBFunctions.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBFunctions {
+    public Connection connect_to_db(String dbname,String username,String password){
+        Connection connection=null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432"+dbname,username,password);
+            if (connection!=null){
+                System.out.println("Connection established");
+            }
+            else{
+                System.out.println("Connection failed");
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+}
