4b8f409f55f62cc51a4e27d787d989c73e43a940
Alexandr Yarmosh <sasha.yarmosh63@gmail.com>
2024-03-26 14:31:19 +0300

    create project

--- src/main/java/org/example/DBFunctions.java
+++ src/main/java/org/example/DBFunctions.java
@@ -1,15 +1,14 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DBFunctions {
     public Connection connect_to_db(String dbname,String username,String password){
-        Connection connection=null;
+        Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432"+dbname,username,password);
-            if (connection!=null){
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+dbname,username,password);
+            if (connection != null){
                 System.out.println("Connection established");
             }
             else{
@@ -20,4 +19,64 @@ public class DBFunctions {
         }
         return connection;
     }
+    public void createTable(Connection connection, String table_name) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            String sql = "create table " + table_name + "(empid SERIAL, name varchar(200), address varchar(200),primary key(empid))";
+            statement.executeUpdate(sql);
+            System.out.println("Table created");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void deleteTable(Connection connection, String table_name) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            String sql = "drop table public." + table_name + ";";
+            statement.executeUpdate(sql);
+            System.out.println("Table deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void readTable(Connection connection, String table_name) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM " + table_name;
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            // Получение метаданных результата запроса
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            // Обработка результатов запроса SELECT
+            while (resultSet.next()) {
+                // Обработка значений каждого столбца
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object value = resultSet.getObject(i);
+
+                    // Вывод данных на консоль или выполнение нужных операций
+                    System.out.println(columnName + ": " + value);
+                }
+            }
+
+            // Закрытие ResultSet и Statement
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    /*    public void updateTable(Connection connection, String table_name) throws SQLException {
+        Statement statement = connection.createStatement();
+        try {
+            String sql = "update table public." + table_name + ";";
+            statement.executeUpdate(sql);
+            System.out.println("Table deleted");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }*/
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DBFunctions db = new DBFunctions();
-        db.connect_to_db("DB_RECORDING_STUDIO", "localhost", "123");
+        Connection connection = db.connect_to_db("DB_RECORDING_STUDIO", "postgres", "123");
+        //db.createTable(connection, "Bobobo");
+        db.readTable(connection,"artist");
     }
 }
