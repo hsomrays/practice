3674bb5bf2469d772e120beb7f81b00dedd87634
Alexandr Yarmosh <sasha.yarmosh63@gmail.com>
2024-03-28 10:43:44 +0300

    cicici

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,14 @@ package org.example;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         DBFunctions db = new DBFunctions();
         Connection connection = db.connect_to_db("DB_RECORDING_STUDIO", "postgres", "123");
         Scanner scanner = new Scanner(System.in);
+        Logger logger = Logger.getLogger(Main.class.getName());
 
         while (true) {
             System.out.println("Choose action:");
@@ -24,35 +26,35 @@ public class Main {
             String tableName;
             switch (choice) {
                 case 1:
-                    System.out.println("Enter the table name you want to create: ");
+                    logger.info("Enter the table name you want to create: ");
                     tableName = scanner.nextLine();
                     db.createTable(connection, tableName);
                     break;
                 case 2:
-                    System.out.println("Enter the table name you want to read: ");
+                    logger.info("Enter the table name you want to read: ");
                     tableName = scanner.nextLine();
                     db.readTable(connection, tableName);
                     break;
                 case 3:
-                    System.out.println("Enter the table name you want to update: ");
+                    logger.info("Enter the table name you want to update: ");
                     tableName = scanner.nextLine();
-                    System.out.println("Enter the column name you want to update: ");
+                    logger.info("Enter the column name you want to update: ");
                     String columnName = scanner.nextLine();
-                    System.out.println("Enter the record id you want to update: ");
+                    logger.info("Enter the record id you want to update: ");
                     int recordId = scanner.nextInt();
                     scanner.nextLine(); // Consume the newline character
-                    System.out.println("Enter new value for the field: ");
+                    logger.info("Enter new value for the field: ");
                     String newValue = scanner.nextLine();
                     db.updateTable(connection, tableName, columnName, newValue, recordId);
                     break;
                 case 4:
-                    System.out.println("Enter table name you want to delete: ");
+                    logger.info("Enter table name you want to delete: ");
                     tableName = scanner.nextLine();
                     db.deleteTable(connection, tableName);
                     break;
                 case 0:
                     // Выход из программы
-                    System.out.println("The program ends.");
+                    logger.info("The program ends.");
                     return;
                 default:
                     System.out.println("Error. Try again");
