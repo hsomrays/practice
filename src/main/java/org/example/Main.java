4b883bdc3bd9a5116596da901d7ab228b5b56a63
Alexandr Yarmosh <sasha.yarmosh63@gmail.com>
2024-03-28 10:23:49 +0300

    cicici

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,28 +19,44 @@ public class Main {
             System.out.println("4. Delete Table");
 
             int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
 
+            String table_name;
             switch (choice) {
                 case 1:
-                    db.createTable(connection, "Bobobo");
+                    System.out.println("Enter the table name you want to create: ");
+                    table_name = scanner.nextLine();
+                    db.createTable(connection, table_name);
                     break;
                 case 2:
-                    db.readTable(connection,"artist");
+                    System.out.println("Enter the table name you want to read: ");
+                    table_name = scanner.nextLine();
+                    db.readTable(connection, table_name);
                     break;
                 case 3:
-                    db.updateTable(connection, "song", "title", "Bobobo sound 2", 6 );
+                    System.out.println("Enter the table name you want to update: ");
+                    table_name = scanner.nextLine();
+                    System.out.println("Enter the column name you want to update: ");
+                    String column_name = scanner.nextLine();
+                    System.out.println("Enter the record id you want to update: ");
+                    int record_id = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline character
+                    System.out.println("Enter new value for the field: ");
+                    String new_value = scanner.nextLine();
+                    db.updateTable(connection, table_name, column_name, new_value, record_id);
                     break;
                 case 4:
-                    db.deleteTable(connection, "Bobobo");
+                    System.out.println("Enter table name you want to delete: ");
+                    table_name = scanner.nextLine();
+                    db.deleteTable(connection, table_name);
                     break;
                 case 0:
                     // Выход из программы
-                    System.out.println("Программа завершается.");
+                    System.out.println("The program ends.");
                     return;
                 default:
                     System.out.println("Error. Try again");
             }
         }
-
     }
 }
