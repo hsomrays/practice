c357c2870e0082f882c991c57a643b23e3716ab2
Alexandr Yarmosh <sasha.yarmosh63@gmail.com>
2024-03-28 14:51:38 +0300

    spring 1

--- src/main/java/com/example/practicespring/repository/AlbumRepository.java
+++ src/main/java/com/example/practicespring/repository/AlbumRepository.java
@@ -4,7 +4,6 @@ import com.example.practicespring.entity.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     
 }
